808c21417c9c6e7540de6ccbb24deafdf016a0d8
AaronM8 <mercadoaaa@gmail.com>
2017-11-01 21:00:21 -0700

    day2 practice

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+// var message = "hellow wolrd!";
+// console.log(typeof message);
+// var john = 23;
+// console.log(typeof john);
+// var bool = true;
+
+
+// function practice(){
+// notDeclared = 'when it works how i expect it to...';
+//
+// var declared = "js is awesome!!";
+//
+// };
+//
+// practice();
+//
+// console.log(notDeclared, declared);
+
+
+// let fresh = true, otherVar = 0;
+
+// console.log(fresh, otherVar);
+
+// const name = "Ryan";
+// console.log(name);
+// if(fresh) {
+//   const name = "Peter";
+//   console.log(name);
+// };
+// console.log(name);
+
+
+// var best = 'Star Wars';
+// let best = 33;
+// const best = 77;
+// console.log(best);
+
+// var rightNow = new Date('');
+//
+// var randomNumber = Math.random();
+//
+// var name = 'John';
+//
+// var stringObj = new String('John');
+
+// console.log(typeof name, typeof stringObj);
+
+// console.log(stringObj.split('o').join('ough'));
+
+let bestNumber = 42;
+
+// console.log(typeof bestNumber);
+
+let reallyBestNumber = new Number(42);
+
+// console.log(reallyBestNumber);
+
+let bool = new Boolean(true);
+
+// console.log(bool);
+
+let object = {
+firstName: 'Stephen',
+lastName: 'Johnstone',
+age:32
+};
+
+// console.log(object);
+
+// alert("hello!");
+// var confirm = confirm("Are you sure?");
+// consol.log(confirm);
+var userAnswer = prompt("What is your answer?");
+consol.log(userAnswer);
