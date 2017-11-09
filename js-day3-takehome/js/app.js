b759bd80c5c174bef1e6a2f04e4bbe60c3a91a40
AaronM8 <mercadoaaa@gmail.com>
2017-11-08 18:22:27 -0800

    takehome3

--- js-day3-takehome/js/app.js
+++ js-day3-takehome/js/app.js
@@ -1,6 +1,16 @@
-var firstName = prompt("John");
-alert(firstName);
-var lastName = prompt("Doe");
-alert(lastName);
+var userName = prompt('What is your name?','Joe');
+var favColor - prompt('What is your favorite color?','Acceptable red, orange, yellow, green, blue, pink, purple, black, white, and grey');
+favColor = favColor.toLowerCase();
+switch(favColor) {
+  case 'red':
+  case 'orange':
+  case 'yellow':
+  case 'green':
+  case 'blue':
+  case 'pink':
+  case 'purple':
+  case 'black':
+  case 'white':
+  case 'grey':
 
-var favoriteColor = prompt()
+}
